f09869739a07a15cc14b6274a141b3dcaea43477
Sanjay Kolisetty <sanjay1409kumar@gmail.com>
2026-01-22 00:05:51 +0530

    ifelse

---
+++ 01_Basics/ifelse.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //Voting age
+    int a;
+    cout<<"age-\n";
+    cin>>a;
+    if(a>=18){
+        cout<<"Eligible to vote";
+    }
+    else{
+        cout<<"Not Eligible to vote";
+    }
+    
+}
