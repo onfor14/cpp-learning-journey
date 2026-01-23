c454835b479e3a5638a95304be5c002502e7be1e
Sanjay Kolisetty <sanjay1409kumar@gmail.com>
2026-01-23 21:51:25 +0530

    inputoutput

---
+++ 01_Basics/inputoutput.cpp
@@ -0,0 +1,13 @@
+//In this I will be taking the input from the user and printing the output what the user have given
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name;
+    int n;
+    cout<<"Enter your name: ";
+    getline(cin, name);
+    cout<<"Enter your age: ";
+    cin>>n;
+    cout<<"Your name is "<<name<<"\nYour age is "<<n<<endl;
+}
