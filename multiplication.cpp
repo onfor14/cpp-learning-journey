9af61215959d1e56883f8f6481c9585f7232544f
Sanjay Kolisetty <sanjay1409kumar@gmail.com>
2026-01-25 23:29:01 +0530

    Added Multiplication Table

---
+++ multiplication.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i=1;
+    cout<<"Enter N: "<<endl;
+    cin>>n;
+    while(i <=n){
+        cout<<n<<"x"<<i<<"="<<n*i<<endl;
+        i++;
+    }
+}
