9e083257bed5fe9d2e43f7d2a9c61ac51a6b371a
Sanjay Kolisetty <sanjay1409kumar@gmail.com>
2026-01-29 22:24:09 +0530

    Pushed Simple_function use program

---
+++ 02_Functions/simple_function.cpp
@@ -0,0 +1,10 @@
+//Here I will be learning how to call a frunction from the main 
+#include<bits/stdc++.h>
+using namespace std;
+void out(){
+    cout<<"Hello world!\nThis output is from the function Out";
+}
+int main()
+{
+    out();
+}
