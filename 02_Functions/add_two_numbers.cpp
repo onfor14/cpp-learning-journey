1acb3d3ec98a6fac6416db1c42c8d3ac22494fbe
Sanjay Kolisetty <sanjay1409kumar@gmail.com>
2026-01-29 22:33:32 +0530

    Added adding of two numbers using functions

---
+++ 02_Functions/add_two_numbers.cpp
@@ -0,0 +1,14 @@
+//Here I wil be adding two numbers using an add function
+#include<bits/stdc++.h>
+using namespace std;
+void add(int a, int b)
+{
+    cout<<"Addition is: "<<a+b<<endl;
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter a and b: "<<endl;
+    cin>>a>>b;
+    add(a,b);
+}
